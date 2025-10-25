496b89f3022eff8502fb9dc2849b37b875efd253
archimehta <mehtaarchi100@gmail.com>
2025-10-25 12:10:59 -0400

    fix main.tsx

--- atticus/client/src/main.tsx
+++ atticus/client/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Prompt from './pages/prompt.tsx'
+import Prompt from './pages/Prompt.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
