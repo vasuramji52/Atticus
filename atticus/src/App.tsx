b21b87fe98fb0e82512e87b400aa4f5daa4ac532
archimehta <mehtaarchi100@gmail.com>
2025-10-25 03:16:00 -0400

    started frontend

--- atticus/src/App.tsx
+++ atticus/src/App.tsx
@@ -8,26 +8,33 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='horizontal-bar'>
+          <link href="https://fonts.googleapis.com/css2?family=Corinthia:wght@400;700&display=swap" rel="stylesheet"></link>
+          <h1>Atticus</h1>
+          <h2>Your personalized legal assistant</h2>
+          <h3>Est. 2025</h3>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='main-body'>
+        <div className="grid-container">
+          <div className="grid-item">BOX 1
+          </div>
+          <div className="grid-item">BOX 2</div>
+          <div className="grid-item">BOX 3</div>
+        </div>
+        <div className='input-box'>
+          <h2>
+            UNSTRUCTURED DATA INPUT
+          </h2>
+          <p>
+            Paste emails, call notes, texts, or any unstructured legal communication. Our AI will extract actionable tasks and route them to specialized assistants.
+          </p>
+          <input className='input-text-box'>
+
+        </input>
+        </div>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
